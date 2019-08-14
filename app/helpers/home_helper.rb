ce89c0b1f33c59c8ab9095af86da74323c8645a0
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2019-08-14 19:48:11 -0300

    Add home page information
    Update the home page view, adding a visual to it.
    
    Add i18n for the application.

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,22 @@
+module HomeHelper
+  # Public: Returns how many non-unique members we have on our groups.
+  #
+  # Returns an Integer value.
+  def members_count
+    330
+  end
+
+  # Public: Returns how many game jams event we were involved.
+  #
+  # Returns an Integer value.
+  def game_jams_count
+    5
+  end
+
+  # Public: Returns how many games were produced on the game jams we were involved.
+  #
+  # Returns an Integer value.
+  def games_count
+    83
+  end
+end
