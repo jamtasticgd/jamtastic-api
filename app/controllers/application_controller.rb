ee35985ee318e5141ef047c6d25e1fff17bbd07e
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2020-09-08 20:12:25 -0300

    Disable CSRF token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_raven_context
+  skip_before_action :verify_authenticity_token, if: :devise_controller?
 
   private
 
