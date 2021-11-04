58a0ebfccfed463814b68fa34958f8584bcf5e80
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2021-11-04 16:36:21 -0300

    Remove assets.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,10 +21,8 @@ Bundler.require(*Rails.groups)
 
 module Jamtastic
   class Application < Rails::Application
-    config.api_only = true
-
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -24,5 +35,7 @@ module Jamtastic
     Raven.configure do |config|
       config.dsn = ENV['SENTRY_DSN']
     end
+
+    config.api_only = true
   end
 end
